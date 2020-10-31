7017f1ca0c12078a3fdc6f8a2a8b45319350b38d
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-10-31 16:41:07 -0300

    add MappingProfile

---
+++ PatrimonioManager/App_Start/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using PatrimonioManager.Dtos;
+using PatrimonioManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PatrimonioManager.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain para Dto
+            Mapper.CreateMap<Marca, MarcaDto>();
+            Mapper.CreateMap<Patrimonio, PatrimonioDto>();
+
+            // Dto para Domain
+            Mapper.CreateMap<MarcaDto, Marca>().ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<PatrimonioDto, Patrimonio>().ForMember(p => p.Id, opt => opt.Ignore());
+        }
+    }
+}
