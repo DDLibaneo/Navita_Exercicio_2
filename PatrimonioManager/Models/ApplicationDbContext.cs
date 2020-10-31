cb299c8153941111c5f55995edf900d284f5256b
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-10-31 12:53:32 -0300

    add ApplicationDbContext

---
+++ PatrimonioManager/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PatrimonioManager.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false) { }
+        
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
