920546a760d65802c621cc1389494bb306812753
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-10-31 16:50:30 -0300

    Add Dtos, add Controllers

---
+++ PatrimonioManager/Controllers/MarcaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Data.Entity;
+using PatrimonioManager.Models;
+using AutoMapper;
+using PatrimonioManager.Dtos;
+
+namespace PatrimonioManager.Controllers
+{
+    public class MarcaController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MarcaController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult GetMarcas(string query = null)
+        {
+            var marcasQuery = (IEnumerable<Marca>)_context.Marcas;
+
+            if (!String.IsNullOrWhiteSpace(query))
+                marcasQuery = marcasQuery.Where(m => m.Nome.Contains(query));
+
+            var marcasDtos = marcasQuery.ToList()
+                .Select(Mapper.Map<Marca, MarcaDto>);
+
+            return Ok();
+        }
+
+
+    }
+}
---
+++ PatrimonioManager/Dtos/PatrimonioDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PatrimonioManager.Dtos
+{
+    public class PatrimonioDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Descricao { get; set; }
+
+        [Required]
+        public int NumeroDoTombo { get; set; }
+
+        public MarcaDto Marca { get; set; }
+
+        [Required]
+        public int MarcaId { get; set; }
+    }
+}
