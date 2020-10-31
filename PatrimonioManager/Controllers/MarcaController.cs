d3710ee2aa01ba2ad9f8236ab6035caa93958822
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-10-31 17:18:33 -0300

    add get, post, put and delete methods to MarcaController

--- PatrimonioManager/Controllers/MarcaController.cs
+++ PatrimonioManager/Controllers/MarcaController.cs
@@ -19,6 +19,7 @@ namespace PatrimonioManager.Controllers
             _context = new ApplicationDbContext();
         }
 
+        // GET /api/marcas
         public IHttpActionResult GetMarcas(string query = null)
         {
             var marcasQuery = (IEnumerable<Marca>)_context.Marcas;
@@ -29,9 +30,71 @@ namespace PatrimonioManager.Controllers
             var marcasDtos = marcasQuery.ToList()
                 .Select(Mapper.Map<Marca, MarcaDto>);
 
-            return Ok();
+            return Ok(marcasDtos);
         }
 
+        // GET /api/marcas/1
+        public IHttpActionResult GetMarca(int id)
+        {
+            var marca = _context.Marcas.SingleOrDefault(m => m.Id == id);
 
+            if (marca == null)
+                return NotFound();
+
+            var marcaDto = Mapper.Map<Marca, MarcaDto>(marca);
+
+            return Ok(marcaDto);
+        }
+
+        // POST /api/marcas
+        [HttpPost]
+        public IHttpActionResult CreateMarca(MarcaDto marcaDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var marca = Mapper.Map<MarcaDto, Marca>(marcaDto);
+
+            _context.Marcas.Add(marca);
+            _context.SaveChanges();
+
+            marcaDto.Id = marca.Id;
+
+            return Ok(marcaDto);
+        }
+
+        // PUT /api/marcas/1
+        [HttpPut]
+        public IHttpActionResult UpdateMarca(int id, MarcaDto marcaDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var marcaInDb = _context.Marcas.SingleOrDefault(m => m.Id == id);
+
+            if (marcaInDb == null)
+                return NotFound();
+
+            Mapper.Map(marcaDto, marcaInDb);
+
+            _context.SaveChanges();
+
+            return Ok(marcaDto);
+        }
+
+        // DELETE /api/marcas/1
+        [HttpDelete]
+        public IHttpActionResult DeleteMarca(int id)
+        {
+            var marcaInDb = _context.Marcas.SingleOrDefault(m => m.Id == id);
+
+            if (marcaInDb == null)
+                return NotFound();
+
+            _context.Marcas.Remove(marcaInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
